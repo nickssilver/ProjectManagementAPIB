02f3759dd912974208495d02387a67d8495258d9
nicks <silvernicks9@gmail.com>
2024-08-19 16:40:39 +0300

    Added Auditlog & settings Models  :heavy_plus_sign:

---
+++ Models/Settings.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjectManagementAPIB.Models
+{
+    public class Settings
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+        public string Profile { get; set; }
+        public string LegalName { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string TaxNo { get; set; }
+        public string Contact { get; set; }
+    }
+}
---
+++ Models/SystemLogs.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjectManagementAPIB.Models
+{
+    public class SystemLogs
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+        public string Username { get; set; }
+        public string ActionType { get; set; }
+        public string EntityName { get; set; }
+        public string IPAddress { get; set; }
+        public string Browser { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public string Description { get; set; }
+
+
+    }
+}
