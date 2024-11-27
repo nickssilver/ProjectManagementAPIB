242541008b085ed480c4681bf1902568adec5e1c
Nicco99 <nicholasnjeru917@gmail.com>
2024-11-27 18:27:04 +0300

    updates on the branch

--- Controllers/ActivityApprovalController.cs
+++ Controllers/ActivityApprovalController.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace ProjectManagementAPIB.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("Api/[controller]")]
     [ApiController]
     public class ActivityApprovalController : ControllerBase
     {
